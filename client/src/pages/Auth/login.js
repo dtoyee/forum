4462064bd162a3a13d918f0d8fd6ed65f970bf32
dtoyee <jongent1992@hotmail.com>
2024-11-02 14:59:44 +0000

    Added login form

--- client/src/pages/Auth/login.js
+++ client/src/pages/Auth/login.js
@@ -1,38 +1,18 @@
+import LoginForm from "../../components/auth/login"
 import Navbar from "../../components/Navbar/navbar"
 
 function Login() {
     return (
         <>
             <Navbar />
-            <main className="container" style={{ marginTop: "20px" }}>
+            <main className="container" style={{ marginTop: "20px", width: "500px" }}>
                 <div class="card">
                     <div class="card-header">
-                        Category
+                        Login
                     </div>
-                        <table className="table table-hover table-bordered">
-                            <thead>
-                                <tr>
-                                    <th style={{ width: "60%" }}>Category</th>
-                                    <th style={{ width: "10%" }} className="text-center">Topics</th>
-                                    <th style={{ width: "10%" }} className="text-center">Posts</th>
-                                    <th style={{ width: "20%" }} className="text-center">Last Post</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>Technology</td>
-                                    <td className="text-center">3</td>
-                                    <td className="text-center">1</td>
-                                    <td className="text-center">Yesterday</td>
-                                </tr>
-                                <tr>
-                                    <td>Technology</td>
-                                    <td className="text-center">3</td>
-                                    <td className="text-center">1</td>
-                                    <td className="text-center">Yesterday</td>
-                                </tr>
-                            </tbody>
-                        </table>
+                    <div className="card-body">
+                        <LoginForm />
+                    </div>
                 </div>
             </main>
         </>
