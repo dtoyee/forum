584783403d513863a66097e32c3f404550daf142
dtoyee <jongent1992@hotmail.com>
2024-11-02 14:49:00 +0000

    Installed browser router

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './pages/Index/index.js'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/Auth/login.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Home />
+  <BrowserRouter>
+    <Routes>
+      <Route path={'/'} element={<Home />} />
+      <Route path={'/login'} element={<Login />} />
+    </Routes>
+  </BrowserRouter>
 );
